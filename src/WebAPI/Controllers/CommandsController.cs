5798396a837f9961c43d88e835aa1d6dcae097c5
Lazar Dilov <ldilov@isimarkets.com>
2021-03-20 00:05:24 +0200

    Initial commit

---
+++ src/WebAPI/Controllers/CommandsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    #region GET Requests
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "CommandLine API";
+        }
+    }
+    
+    #endregion
+}
