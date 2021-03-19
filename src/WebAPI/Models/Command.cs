66b6f163021e4a7fc91079f308f0b0ac785cec60
Lazar Dilov <ldilov@isimarkets.com>
2021-03-20 00:09:27 +0200

    Added Command Model

---
+++ src/WebAPI/Models/Command.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Platform { get; set; }
+        
+        [Required]
+        [MaxLength(150)]
+        public string CommandLine { get; set; }
+    }
+}
